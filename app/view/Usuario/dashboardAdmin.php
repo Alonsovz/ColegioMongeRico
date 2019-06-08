6247f4b73ce43944279f3e24696f3b71499b4a93
unknown <fabiomejiash@gmail.com>
2019-06-08 09:54:18 -0600

    ok

--- app/view/Usuario/dashboardAdmin.php
+++ app/view/Usuario/dashboardAdmin.php
@@ -18,7 +18,37 @@ $(function() {
         <i class="users icon"></i><i class="book icon"></i>
     </a>
 
-    <a href="?1=RemesasController&2=chequeras" style="width: 24%;"  class="tiles-tiles ui blue inverted segment">
+    <a href="?1=PagosController&2=primerGrado"  style="width: 24%;" class="tiles-tiles ui black inverted segment">
+        <h3>Control de pagos</h3>
+        <div class="ui divider"></div>
+        <i class="dollar icon"></i><i class="book icon"></i>
+    </a>
+
+    <a href="?1=GestionGradosController&2=primerGrado" style="width: 24%;"  class="tiles-tiles ui purple inverted segment">
+        <h3>Control de notas</h3>
+        <div class="ui divider"></div>
+        <i class="book icon"></i><i class="pencil icon"></i>
+    </a>
+
+
+    <a href="?1=SolicitudController&2=fichaAlumno" style="width: 24%;"  class="tiles-tiles ui olive inverted segment">
+        <h3>Nueva Ficha Alumno</h3>
+        <div class="ui divider"></div>
+        <i class="plus icon"></i><i class="user icon"></i>
+    </a>
+
+    
+</div>
+
+
+<div class="row tiles" id="contenedor-tiles" style="display: flex !important; align-items: baseline; justify-content: space-between">
+<a href="?1=InventarioController&2=calcetas" style="width: 24%;"  class="tiles-tiles ui yellow inverted segment">
+        <h3>Inventario</h3>
+        <div class="ui divider"></div>
+        <i class="list icon"></i><i class="pencil icon"></i>
+    </a>
+
+<a href="?1=RemesasController&2=chequeras" style="width: 24%;"  class="tiles-tiles ui blue inverted segment">
         <h3>Cuentas de Banco</h3>
         <div class="ui divider"></div>
         <i class="dollar icon"></i><i class="money bill icon"></i>
@@ -35,4 +65,8 @@ $(function() {
         <div class="ui divider"></div>
         <i class="save icon"></i>
     </a>
+   
+
+    
 </div>
+
